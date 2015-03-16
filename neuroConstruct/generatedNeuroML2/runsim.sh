4537241ed1d962d3940a2c8fee1b8a48bd4996f6
Padraig Gleeson <p.gleeson@gmail.com>
2015-03-16 16:33:58 +0000

    Adding more chans in nml2 & renamed main large cell

--- neuroConstruct/generatedNeuroML2/runsim.sh
+++
@@ -1,2 +0,0 @@
-export JNML_HOME=/home/padraig/neuroConstruct/jNeuroMLJar
-/home/padraig/neuroConstruct/jNeuroMLJar/jnml /home/padraig/neuroConstruct/osb/hippocampus/CA1_pyramidal_neuron/CA1PyramidalCell/neuroConstruct/generatedNeuroML2/LEMS_CA1PyramidalCell.xml
